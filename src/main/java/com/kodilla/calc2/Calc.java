31b9e1ce9c87d02dddd1f2a87a393dbd56d9a434
Meham85 <codedtp@gmail.com>
2019-05-10 11:59:55 +0200

    First Commit

---
+++ src/main/java/com/kodilla/calc2/Calc.java
@@ -0,0 +1,13 @@
+package com.kodilla.calc2;
+
+public class Calc {
+
+        public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+        public int subtractAFromB(int a, int b) {
+        return a - b;
+    }
+    }
+
